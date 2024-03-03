f26e1d6727b9c5d154b78a2a99ac9e444ef44fb7
bulletkid1 <151473536+bulletkid1@users.noreply.github.com>
2024-03-03 22:21:18 +0100

    TP2

---
+++ src/main/java/com/eniso/tp2i/TP2i.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.eniso.tp2i;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class TP2i {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
